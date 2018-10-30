114b9bb8bef02d60d6b0982673053d75848e9eaa
Matija Bačić <matijabacic98@gmail.com>
2018-10-30 20:52:08 +0100

    bug fixes

--- dretve.c
+++ dretve.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/shm.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <pthread.h>
 
 int A = 0;
 
-void task(int m){
- 
+void *task(int m){
+
     int i;
     for(i=0;i<m;i++){
         A++;
     }
-    
+
 }
 
 int main(int argc, char** argv){
-    
+
     if(argc!=3)
         exit(1); /* greška - krivi broj argumenata */
- 
+
     int n = atoi(argv[1]);
     int m = atoi(argv[2]);
-    
+
+    pthread_t *tid = malloc(sizeof(pthread_t)*n);
     int i;
     for(i=0;i<n;i++){
-        if(fork()==0){
-            task(m);
+        if(pthread_create(&tid[i], NULL, task, &m)!=0){
+            printf("[ERROR] Can not create thread %d",i+1);
             exit(0);
         }
     }
-    
-    while(i--) wait(NULL);
-    
-    printf("A=%d\n",*A);
-    
+
+    while(i--) pthread_join(tid[i], NULL);
+
+    printf("A=%d\n",A);
+
 }
--- procesi.c
+++ procesi.c
@@ -8,42 +8,52 @@ int id; /* identifikacijski broj segmenta */
 int *A;
 
 void task(int m){
- 
+
     int i;
     for(i=0;i<m;i++){
         *A = (*A)+1;
     }
-    
+
 }
 
 int main(int argc, char** argv){
-    
+
     if(argc!=3)
         exit(1); /* greška - krivi broj argumenata */
-    
+
     /* zauzimanje zajedničke memorije */
     id = shmget(IPC_PRIVATE, sizeof(int), 0600);
- 
+
     if (id == -1)
         exit(1);  /* greška - nema zajedničke memorije */
- 
+
     A = (int *) shmat(id, NULL, 0);
     *A = 0;
- 
+
     int n = atoi(argv[1]);
     int m = atoi(argv[2]);
-    
+
     int i;
     for(i=0;i<n;i++){
-        if(fork()==0){
-            task(m);
-            shmdt(char *addr);
-            exit(0);
+        switch(fork()){
+		case 0:
+            		task(m);
+            		shmdt(A);
+            		exit(0);
+		case -1:
+			printf("[ERROR] Can not create process number %d",i+1);
+		case 1:
+			break;
+		default:
+			break;
         }
     }
-    
+
     while(i--) wait(NULL);
-    
+
     printf("A=%d\n",*A);
-    
+    shmctl(id,IPC_RMID,NULL);
+
+    return 0;
+
 }
